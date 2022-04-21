bab0b894a5451b0a006af697df4f3b03e190999b
kasandradarwin <86120308+kasandradarwin@users.noreply.github.com>
2022-04-21 12:04:32 -0700

    Completed Chapter 5 exercise 1
    added exception handling

--- playground.py
+++ playground.py
@@ -1,24 +1,3 @@
-num = None
-largest = None
-smallest = None
-
-while True:
-    num = input("Enter a number: ")
-    if num == "done":
-        break
-    try:
-        num = int(num)
-        if largest == None:
-            largest = num
-        elif num > largest:
-            largest = num
-        if smallest == None:
-            smallest = num
-        elif num < smallest:
-            smallest = num
-
-    except:
-        print('Invalid input')
-
-print("Maximum is", largest)
-print("Minimum is", smallest)
+for i in [1, 2, 3, 4, 5]:
+    print(i)
+print('Blastoff!')
