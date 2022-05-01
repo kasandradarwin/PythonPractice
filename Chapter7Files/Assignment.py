fdcdc0a6c915feeadce9170c75b9a6845cbb00f3
kasandradarwin <86120308+kasandradarwin@users.noreply.github.com>
2022-05-01 07:57:14 -0700

    Added exception handling

--- Chapter7Files/Assignment.py
+++ Chapter7Files/Assignment.py
@@ -1,6 +1,10 @@
 # Use words.txt as the file name
 fname = input("Enter file name: ")
-fh = open(fname)
+try:
+	fh = open(fname)
+except:
+    print("Sorry, we can't find fname. Please try again.")
+    quit()
 contents = fh.read()
 upper_contents = contents.upper().strip()
 print(upper_contents)
