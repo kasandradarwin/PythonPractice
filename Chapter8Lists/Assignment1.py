8cd8691690804fdcfaef94a66bce3d5e4172e784
kasandradarwin <86120308+kasandradarwin@users.noreply.github.com>
2022-05-08 18:52:10 -0700

    completed assignment, fixed the list builder

--- Chapter8Lists/Assignment1.py
+++ Chapter8Lists/Assignment1.py
@@ -12,13 +12,14 @@ try:
 except:
     print(fname, "is not a valid file name. Please try again")
     quit()
+lst = list()
 for line in fh:
-    lst = list()
     line = line.rstrip()
-    word = line.split()
-    word.sort()
-    if word not in line:
-        lst.append()
+    words = line.split()
+    for word in words:
+        if word in lst:
+            continue
+        lst.append(word)
+        lst.sort()
 
-
-print(word)
+print(lst)
--- playground.py
+++ playground.py
@@ -1,3 +1,12 @@
-fruit = 'Banana'
-fruit[0] = 'b'
-print(fruit)
+items = ['d', 'b', 'c', 'a']
+def chop():
+    print(items[1:3])
+
+def middle(x):
+    del x[1:3]
+    print(x)
+
+print(sorted(items))
+chop()
+middle(items)
+print(sorted(items))
