e70c511f38ab7f6a43bbf1976895810a213ac88f
kasandradarwin <86120308+kasandradarwin@users.noreply.github.com>
2022-05-16 18:22:16 -0700

    fixed count

--- Chapter9Dictionaries/assignment1.py
+++ Chapter9Dictionaries/assignment1.py
@@ -1,9 +1,10 @@
-#9.4 ✅Write a program to read through the mbox-short.txt and
-#figure out who has sent the greatest number of mail messages.
-#The program looks for 'From ' lines and
-#takes the second word of those lines as the person who sent the mail.
-#The program creates a Python dictionary that maps the sender's mail address to a
+#9.4 Write a program to read through the mbox-short.txt and figure out who has sent the greatest number of mail messages.
+
+#✅The program looks for 'From ' lines and
+#✅takes the second word of those lines as the person who sent the mail.
+#✅The program creates a Python dictionary that maps the sender's mail address to a
 #count of the number of times they appear in the file.
+
 #After the dictionary is produced, the program reads through the dictionary
 #using a maximum loop to find he most prolific committer.
 
@@ -18,11 +19,8 @@ counter = dict()
 for line in handle:
     if not line.startswith('From'):
         continue
-    words = line.split()[1]
-    print("line:",line)
+    words = line.split()
+    words = words[1:2]
+
     for word in words:
-        counter[word] = counter.get(word, 0) + 1
-        print(counter[word])
-    #if not line.startswith("From"):
-        #continue
-    #line = line.rstrip()
+        counter[word] = counter.get(word,1) +1
