3fe3367aa8eea316cb5270264499490f1ea01655
kasandradarwin <86120308+kasandradarwin@users.noreply.github.com>
2022-05-30 19:18:11 -0700

    Added some debugging print statements

--- Chapter11RegEx/ex_1.py
+++ Chapter11RegEx/ex_1.py
@@ -3,17 +3,24 @@
 
 import re
 
-x = input(" Please enter a RegEx, eg '^Author': ")
+x = input("Please enter a RegEx, eg '^Author': ")
 file = open("mbox-short.txt")
 count = 0
 try:
     for line in file:
         line = line.rstrip()
+        print("line: ",line)
+        print ("x: ", x)
         result = re.findall(x,line)
-        for match in line:
-            if len(result) > 0:
-                count = count + 1
-    print("mbox.txt had", count, "lines that matched", x)
+        #print("result: ",result)
+        for word in line:
+            print("count pre len: ", count)
+            if len(line) > 0:
+                count += 1
+                print("count post len: ", count)
+
+    #print("mbox.txt had", count, "lines that matched", x)
+
 
 
 except:
