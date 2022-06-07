0da43980012b9b4a0b64a93b0860a6cfc1cc4b8a
kasandradarwin <86120308+kasandradarwin@users.noreply.github.com>
2022-06-06 18:45:19 -0700

    working on webscraper

---
+++ Chapter12NetworkedPrograms/week4assignment1.py
@@ -0,0 +1,35 @@
+#In this assignment you will write a Python program to use urllib to read the HTML
+#from the data files below, and parse the data, extracting numbers and compute the
+#sum of the numbers in the file
+
+
+
+
+#To run this, download the BeautifulSoup zip file
+# http://www.py4e.com/code3/bs4.zip
+# and unzip it in the same directory as this file
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, "html.parser")
+
+
+# Retrieve all of the anchor tags
+tags = soup('span')
+for tag in tags:
+    # Look at the parts of a tag
+    print(line.decode().strip())
+    print('TAG:', tag)
+    print('URL:', tag.get('href', None))
+    print('Contents:', tag.contents[0])
+    print('Attrs:', tag.attrs)
