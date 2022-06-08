8088e02cbaa5a71844ffa8dc8c2ae8ef5dfc2795
kasandradarwin <86120308+kasandradarwin@users.noreply.github.com>
2022-06-07 20:50:51 -0700

    adding position and counts

--- Chapter12NetworkedPrograms/week4assignment2.py
+++ Chapter12NetworkedPrograms/week4assignment2.py
@@ -15,7 +15,29 @@ url = input('Enter - ')
 html = urllib.request.urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, 'html.parser')
 
+count = 0
+countInput = input('Enter Count: ')
+positionInput = input('Enter position: ')
+
+
 # Retrieve all of the anchor tags
 tags = soup('a')
-for tag in tags:
-    print(tag.get('href', None))
+while count < countInput:
+    for tag in tags:
+        if position != positionInput:
+            continue
+            count += 1
+            #print("COUNT: ", count)
+            #print(tag.get('href', None))
+            #print('TAG:', tag)
+            #print('URL:', tag.get('href', None))
+            print('Contents:', tag.contents[3])
+            #print('Attrs:', tag.attrs)
+
+#tags = soup('span')
+#sum = 0
+#tagSum = 0
+#for tag in tags:
+#    tagSum = tag.contents[0]
+#    sum += int(tagSum)
+#print("sum: ", sum)
