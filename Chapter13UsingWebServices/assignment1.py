dbf5216ab9a8fdd64916270938c5bee8b8add1e2
kasandradarwin <86120308+kasandradarwin@users.noreply.github.com>
2022-06-12 19:59:15 -0700

    changed count to len

--- Chapter13UsingWebServices/assignment1.py
+++ Chapter13UsingWebServices/assignment1.py
@@ -2,14 +2,9 @@ import urllib.request, urllib.parse, urllib.error
 import xml.etree.ElementTree as ET
 import ssl
 
-#api_key = False
-# If you have a Google Places API key, enter it here
-# api_key = 'AIzaSy___IDByT70'
-# https://developers.google.com/maps/documentation/geocoding/intro
-
-#url = 'http://py4e-data.dr-chuck.net/comments_42.xml'
+url = 'http://py4e-data.dr-chuck.net/comments_42.xml'
 #url = 'http://py4e-data.dr-chuck.net/comments_1527516.xml'
-url = input("Enter a URL -")
+#url = input("Enter a URL -")
 print('Retrieving:', url)
 
 
@@ -32,12 +27,11 @@ tree = ET.fromstring(linkOpener.read())
 #initialize variable to found each iteration, and sum all the numbers
 countList = tree.findall('.//count')
 sum = 0
-numberofitems = 0
 
 # for every item in the list, pull the value out, count the number of iterations
 for item in countList:
     eachCount = (item.text)
-    numberofitems += 1
     sum = sum + int(eachCount)
-print("Count: ", numberofitems)
+
+print("Count: ", len(countList))
 print("Sum: ", sum)
