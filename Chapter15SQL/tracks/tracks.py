f26f15c9cfffb594469faaabcb7743a693e30228
kasandradarwin <86120308+kasandradarwin@users.noreply.github.com>
2022-06-25 11:38:55 -0700

    Started fresh, fixed bug

--- Chapter15SQL/tracks/tracks.py
+++ Chapter15SQL/tracks/tracks.py
@@ -7,18 +7,13 @@ cur = conn.cursor()
 # Make some fresh tables using executescript()
 cur.executescript('''
 DROP TABLE IF EXISTS Artist;
-DROP TABLE IF EXISTS Genre;
 DROP TABLE IF EXISTS Album;
+DROP TABLE IF EXISTS Genre;
 DROP TABLE IF EXISTS Track;
 
 CREATE TABLE Artist (
     id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
     name    TEXT UNIQUE
-);
-
-CREATE TABLE Genre (
-    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-    name
 );
 
 CREATE TABLE Album (
@@ -27,11 +22,18 @@ CREATE TABLE Album (
     title   TEXT UNIQUE
 );
 
+CREATE TABLE Genre (
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    artist_id  INTEGER,
+    name   TEXT UNIQUE
+);
+
 CREATE TABLE Track (
     id  INTEGER NOT NULL PRIMARY KEY
         AUTOINCREMENT UNIQUE,
-    title TEXT  UNIQUE,
+    title TEXT UNIQUE,
     album_id  INTEGER,
+    genre_id INTEGER,
     len INTEGER, rating INTEGER, count INTEGER
 );
 ''')
@@ -53,7 +55,7 @@ def lookup(d, key):
     return None
 
 stuff = ET.parse(fname)
-all = stuff.findall('dict/dict/dict/dict')
+all = stuff.findall('dict/dict/dict')
 print('Dict count:', len(all))
 for entry in all:
     if ( lookup(entry, 'Track ID') is None ) : continue
@@ -65,30 +67,31 @@ for entry in all:
     count = lookup(entry, 'Play Count')
     rating = lookup(entry, 'Rating')
     length = lookup(entry, 'Total Time')
+    print("genre", genre)
 
-    if name is None or artist is None or album is None or genre is None :
+    if name is None or artist is None or album is None:
         continue
 
-    print(name, artist, genre, album, count, rating, length)
+    print(name, artist, album, genre, count, rating, length)
 
     cur.execute('''INSERT OR IGNORE INTO Artist (name)
         VALUES ( ? )''', ( artist, ) )
     cur.execute('SELECT id FROM Artist WHERE name = ? ', (artist, ))
     artist_id = cur.fetchone()[0]
 
-    cur.execute('''INSERT OR IGNORE INTO Genre (genre)
-        VALUES ( ? )''', ( genre, artist_id  ) )
-    cur.execute('SELECT id FROM Genre WHERE genre = ? ', (genre, ))
-    genre_id = cur.fetchone()[0]
-
     cur.execute('''INSERT OR IGNORE INTO Album (title, artist_id)
-        VALUES ( ?, ? )''', ( album, genre_id, artist_id ) )
+        VALUES ( ?, ? )''', ( album, artist_id ) )
     cur.execute('SELECT id FROM Album WHERE title = ? ', (album, ))
     album_id = cur.fetchone()[0]
 
+    cur.execute('''INSERT OR IGNORE INTO Genre (name)
+        VALUES ( ? )''', ( genre, ) )
+    cur.execute('SELECT id FROM Genre WHERE name = ? ', (genre, ))
+    genre_id = cur.fetchone()[0]
+
     cur.execute('''INSERT OR REPLACE INTO Track
-        (title, album_id, len, rating, count)
+        (title, album_id, len, genre_id, rating, count)
         VALUES ( ?, ?, ?, ?, ?, ? )''',
-        ( name, genre, album_id, length, rating, count ) )
+        ( name, album_id, length, genre_id, rating, count ) )
 
     conn.commit()
